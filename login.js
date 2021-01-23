ca6d544e387bf37a038bd507677a3f0e516f9646
Ernest Salim <salim.ernest17@gmail.com>
2021-01-23 10:05:08 +0700

    Second commit
    - Error design
    - Text/title changing
    - Contact header
    - Password placeholder

--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+// PASSWORD INPUT HANDLING
 let passwordToggle = document.querySelectorAll('.password-toggle');
 let formPassword = document.querySelectorAll('.input-password')
 
@@ -20,6 +21,7 @@ for (let i = 0; i < passwordToggle.length; i++) {
     })
 }
 
+// PAGE TOGGLE HANDLING
 let signupTrigger = document.querySelector('.sign-up-trigger');
 let signinTrigger = document.querySelector('.sign-in-trigger');
 let page = document.querySelector('.page-base');
@@ -28,13 +30,16 @@ let contentBG = document.querySelector('.form-content');
 signupTrigger.addEventListener('click', () => {
     page.classList.add('page-alter');
     page.style.animation = "backgroundChanger1 1s ease-in-out forwards";
+    error.style.display = "none";
 });
 
 signinTrigger.addEventListener('click', () => {
     page.classList.remove('page-alter');
     page.style.animation = "backgroundChanger2 1s ease-in-out forwards";
+    error.style.display = "none";
 });
 
+// MENU TOGGLE
 function toggleMenu() {
     var menuToggle = document.querySelector('.toggle');
     var navigation = document.querySelector('.navigation');
@@ -42,6 +47,7 @@ function toggleMenu() {
     navigation.classList.toggle('active');
 }
 
+// AGE INPUT HANDLING
 const age = document.getElementById("age");
 
 age.oninput = function () {
@@ -49,3 +55,22 @@ age.oninput = function () {
         this.value = this.value.slice(0, 2);
     }
 }
+
+// ERROR HANDLING
+let signinButton = document.querySelector(".sign-in-button");
+let signupButton = document.querySelector(".sign-up-button");
+
+let error = document.querySelector(".error");
+let error_text = document.querySelector(".error-text");
+let errorClose = document.querySelector('.error-circle');
+let errorBox = document.querySelector('.error');
+
+errorClose.addEventListener('click', () => {
+  errorBox.style.transform = "scale(0.01)";
+  errorBox.style.opacity = "0";
+  if (errorBox.style.opacity === "0") {
+    errorBox.style.display = "none";
+  }
+});
+
+
